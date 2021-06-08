bbbbfb3d1a2e6c090c14f22706cf17e09b829b90
Kevin Zhu <zyx97agm@gmail.com>
2021-06-08 09:39:01 +0000

    wire different components

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,7 +1,24 @@
-import React from "react"
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-const App = ()=>{
-	return <div>App</div>
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
+
+const App = () => {
+	return (
+		<div>
+			<BrowserRouter>
+				<Route path="/" exact component={StreamList} />
+				<Route path="/streams/new" exact component={StreamCreate} />
+				<Route path="/streams/edit" exact component={StreamEdit} />
+				<Route path="/streams/delete" exact component={StreamDelete} />
+				<Route path="/streams/show" exact component={StreamShow} />
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
