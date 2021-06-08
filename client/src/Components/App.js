3b70c50021b54950c1b9953b5db135b71b8f0af0
Kevin Zhu <zyx97agm@gmail.com>
2021-06-08 11:06:35 +0000

    first commit for router

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -6,16 +6,20 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
+import Header from './Header';
 
 const App = () => {
 	return (
-		<div>
+		<div className="ui container">
 			<BrowserRouter>
-				<Route path="/" exact component={StreamList} />
-				<Route path="/streams/new" exact component={StreamCreate} />
-				<Route path="/streams/edit" exact component={StreamEdit} />
-				<Route path="/streams/delete" exact component={StreamDelete} />
-				<Route path="/streams/show" exact component={StreamShow} />
+				<div>
+					<Header />
+					<Route path="/" exact component={StreamList} />
+					<Route path="/streams/new" exact component={StreamCreate} />
+					<Route path="/streams/edit" exact component={StreamEdit} />
+					<Route path="/streams/delete" exact component={StreamDelete} />
+					<Route path="/streams/show" exact component={StreamShow} />
+				</div>
 			</BrowserRouter>
 		</div>
 	);
