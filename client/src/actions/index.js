0c15e11ef6fc58fb0320b1188637b8a362daade4
yuxuan <zyx97agm@gmail.com>
2021-06-16 10:49:29 +0000

    finish stream reducer

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,18 @@
-import {SIGN_IN, SIGN_OUT, CREATE_STREAM} from "./types"
-import streams from "../apis/streams"
+import {
+	SIGN_IN,
+	SIGN_OUT,
+	CREATE_STREAM,
+	DELETE_STREAM,
+	FETCH_STREAM,
+	FETCH_STREAMS,
+	EDIT_STREAM,
+} from './types';
+import streams from '../apis/streams';
 
 export const signIn = (userId) => {
 	return {
 		type: SIGN_IN,
-		payload: userId
+		payload: userId,
 	};
 };
 
@@ -16,13 +24,46 @@ export const signOut = () => {
 
 // make network request to api server and save the new
 // created stream to the server.
-export const createStream = (formValues) =>{
-	return async(dispatch)=>{
-		const {data} = await streams.post("/streams", formValues)
+export const createStream = (formValues) => {
+	return async (dispatch) => {
+		const response = await streams.post('/streams', formValues);
+		console.log(response)
 		dispatch({
 			type: CREATE_STREAM,
-			payload: data
-		})
-	}
-}
+			payload: response.data,
+		});
+	};
+};
+
+// action creator to fetch all streams from api server
+export const fetchStreams = () => {
+	return async (dispatch) => {
+		const response = await streams.get('/streams');
+		dispatch({ type: FETCH_STREAMS, payload: response.data });
+	};
+};
+
+// action creator to fetch one specific stream using the id from api server
+export const fetchStream = (id) => {
+	return async (dispatch) => {
+		const response = await streams.get(`/streams/${id}`);
+		dispatch({ type: FETCH_STREAM, payload: response.data });
+	};
+};
+
+// action creator to delete one specific stream using the id from api server
+export const deleteStream = (id) => {
+	return async (dispatch) => {
+		await streams.delete(`/streams/${id}`);
+		dispatch({ type: DELETE_STREAM, payload: id });
+	};
+};
+
+// action creator to update one specific stream using the id from api server
+export const editStream = (id, formValues) => {
+	return async (dispatch) => {
+		const response = await streams.put(`/streams/${id}`, formValues);
+		dispatch({ type: EDIT_STREAM, payload: response.data });
+	};
+};
 
