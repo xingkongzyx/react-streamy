129efcc82d1fc2e2d902dc23246b923515bc5a31
yuxuan <zyx97agm@gmail.com>
2021-06-20 14:08:13 +0000

    refactor create form

--- client/src/Components/streams/StreamCreate.js
+++ client/src/Components/streams/StreamCreate.js
@@ -1,81 +1,24 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
+import StreamForm from './StreamForm';
 import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
-	// 	辅助renderInput function显示error
-	renderErrors(meta) {
-		const { error, touched } = meta;
-		// 如果用户已经touch了input并且确实存在error，则显示给user
-		if (touched && error) {
-			return (
-				<div className="ui error message">
-					<div className="header">{error}</div>
-				</div>
-			);
-		}
-	}
-
-	renderInput = (formProps) => {
-		const { label, input, meta } = formProps;
-		return (
-			<div>
-				<label>{label}</label>
-				<input {...input} autoComplete="off" />
-				{this.renderErrors(meta)}
-			</div>
-		);
-	};
-
 	// 	redux-form中的handleSubmit处理完表格提交后调用的callback function
 	// 	参数为提交时表格内输入的values,我们不再需要event object
-	onFormSubmit = (formValues) => {
+	onCreateFormSubmit = (formValues) => {
 		this.props.createStream(formValues);
 	};
 
 	render() {
 		return (
-			<div className="field">
-				<form
-					className="ui form error"
-					onSubmit={this.props.handleSubmit(this.onFormSubmit)}
-				>
-					<Field name="title" component={this.renderInput} label="Enter Title" />
-					<Field
-						name="description"
-						component={this.renderInput}
-						label="Enter Description"
-					/>
-					<button className="ui button primary">Submit</button>
-				</form>
+			<div>
+				<h3>Edit Form</h3>
+				<StreamForm onSubmit={this.onCreateFormSubmit}></StreamForm>
 			</div>
 		);
 	}
 }
 
-// 处理表格提交时的validation
-const validate = (formValues) => {
-	const errors = {};
-	// 	如果title为空
-	if (!formValues.title) errors.title = 'You must enter a title';
-	// 	如果description为空
-	if (!formValues.description) errors.description = 'You must enter a description';
-	// 	最后返回errors object(可能为空或者包含对应的error)
-	return errors;
-};
-
-// wire up redux-form with the component
-// 这样component的props中便会自动包含很多相关的属性
-// 注意第一个function的所有参数都以key-value形式在{ form: 'streamCreate' }出现
-const wrappedStream = reduxForm({
-	form: 'streamCreate',
-	validate,
-})(StreamCreate);
 // Wire up the connect function with the component
-export default connect(null, { createStream })(wrappedStream);
-
-
-
-
-
+export default connect(null, { createStream })(StreamCreate);
