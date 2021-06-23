95abad563cf4ef1feb3aefb6c4fc436701d5a6df
yuxuan <zyx97agm@gmail.com>
2021-06-23 13:02:50 +0000

    create modal component

--- client/src/Components/streams/StreamDelete.js
+++ client/src/Components/streams/StreamDelete.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React from 'react';
+import Modal from '../Modal';
 
-const StreamDelete = ()=>{
-	return <div>StreamDelete</div>
-} 
+
+
+const StreamDelete = () => {
+	return (
+		<div>
+			StreamDelete 
+			<Modal />
+		</div>
+	);
+};
 
 export default StreamDelete;
